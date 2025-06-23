f1f51bda8e0c97c0ec37a525337f4c282764c70f
p1nkRiv333r <158548974+p1nkRiv333r@users.noreply.github.com>
2025-06-24 04:44:38 +0700

    Create Contact Support

---
+++ src/java/Model/Contact.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Admin
+ */
+public class Contact {
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String subject;
+    private String message;
+    private Timestamp createdAt;
+
+    public Contact() {
+    }
+
+    public Contact(int id, String name, String email, String phoneNumber, String subject, String message, Timestamp createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.subject = subject;
+        this.message = message;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    
+    
+}
